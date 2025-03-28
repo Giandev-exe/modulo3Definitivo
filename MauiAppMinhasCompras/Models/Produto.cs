c1867f078b11c89e6cf09fa836522f51b9afdc53
COBR4 <COBR4@DESKTOP-65CVB5U>
2025-03-28 19:35:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        String _descricao;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { 
+            get => _descricao;
+            set 
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descricao");
+                }
+                else
+                {
+                    _descricao = value;
+                }
+            } 
+        }
+        public double Quantidade {  get; set; }
+        public double Preco {  get; set; }
+        public double Total { get => Quantidade * Preco; }
+    }
+}
